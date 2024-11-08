3c99ac05ea0c9a1061be73335a974818f439e70f
Sanjana <67232409+koiralasanjana@users.noreply.github.com>
2024-11-08 06:52:00 +0545

    project initialization

---
+++ src/main/java/com/herpath/ai_stem_tutor/AiPoweredStemTutorApplication.java
@@ -0,0 +1,13 @@
+package com.herpath.ai_stem_tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredStemTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredStemTutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herpath/ai_stem_tutor/AiPoweredStemTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herpath.ai_stem_tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPoweredStemTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
